ab5bd91fde61c44ef2671fd02893142b83305d88
ramcj7742 <ramcj7742@wrdsb.ca>
2020-10-06 14:05:15 +0000

    Finished factorial problem

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * Calculates factoral of user number
+ * @author Jaden Ramcharan
  */
 public class Main {
 
@@ -9,7 +10,27 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // creates scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    //gets number from user
+    System.out.println("Please enter an integer to calculate the factorial of");
+    int userNum = input.nextInt();
+
+    //declare accumulator and factorial variable
+    int factorial = 0;
+    int acc = userNum;
+
+    //Start calculating factorial
+    for(int i = userNum; i > 1; i--){
+     factorial = acc * (i - 1);
+     acc = factorial;
     
+    }
+    //tells user factorial
+    if(userNum == 0){
+      System.out.println(userNum + "! = 1");
+    }else
+    System.out.println(userNum + "! = " + acc);
   }
 }
